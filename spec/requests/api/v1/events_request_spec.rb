7b345d03f46b583542628b89837d3c3642e4e209
Tory Dannenberg <rfdberg@gmail.com>
2019-01-21 14:25:01 -0700

    Add tests for getting events and add actions to pass tests

--- spec/requests/api/v1/events_request_spec.rb
+++ spec/requests/api/v1/events_request_spec.rb
@@ -4,8 +4,20 @@ describe "Events API" do
   it "sends a list of events" do
     create_list(:event, 3)
 
-    get '/api/v1/events'
+    get "/api/v1/events"
+    puts response.body
+    expect(response).to be_successful
+
+    events = JSON.parse(response.body)
+  end
+
+  it "can get one event by its id" do
+    id = create(:event).id
+    get "/api/v1/events/#{id}"
+
+    event = JSON.parse(response.body)
 
     expect(response).to be_successful
+    expect(event["id"]).to eq(id)    
   end
 end  
